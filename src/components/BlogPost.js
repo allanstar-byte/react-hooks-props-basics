1b86577fb639f78b55837ec3ae5b39b68b19e82a
allanstar-byte <allanmutisya263@gmail.com>
2022-10-18 13:30:35 +0300

    Props

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -9,6 +9,11 @@ function BlogPost() {
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText="Testing things" />
+      <Comment commentText="I agree with this statement. - Angela Merkel" />
+      <Comment commentText="A universal truth. - Noam Chomsky" />
+      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText="Testing things" />
     </div>
   );
 }
